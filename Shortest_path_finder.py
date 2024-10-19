4fdc656c71fcd18218aca8111a034d2541725b32
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2024-10-19 22:51:01 +0530

    Add files via upload

---
+++ Shortest_path_finder.py
@@ -0,0 +1,19 @@
+# This program is going to find the shortest path by itself in the maze.
+# In this we are going to use window-curses module which is used for TUI (text based user interface)
+# in terminal application.
+
+import curses
+from curses import wrapper
+import queue
+import time
+maze = [
+    ["#", "O", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", " ", "#", "#", " ", "#", "#", " ", "#"],
+    ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
+]
