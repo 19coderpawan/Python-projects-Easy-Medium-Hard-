f082f400e210885cd2daff96bb7538f46a2e5af2
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2024-10-20 20:06:24 +0530

    Update Shortest_path_finder.py

--- Shortest_path_finder.py
+++ Shortest_path_finder.py
@@ -1,6 +1,6 @@
 # This program is going to find the shortest path by itself in the maze.
 # In this we are going to use window-curses module which is used for TUI (text based user interface)
-# in terminal application.
+# in terminal application. In this we are using Breadth First Search (BFS) for locating the shortest path.
 
 '''Imp Note-: Sometimes your IDE built in terminal does not support curses module , in that case
                trying using your Windows or linux terminal like CMD for windows.In this simply get in
@@ -34,6 +34,25 @@ def print_maze(maze,stdscr,path=[]):
         for j , value in enumerate(row):
             stdscr.addstr(i,j*2,value,green)
 
+
+# function to find the starting position.
+def find_start(maze,start):
+    for i , row in enumerate(maze):
+        for j , value in enumerate(row):
+            return i , j
+    return None
+
+# function to find the path.
+def find_path(maze):
+    #initlize the start and the end of the maze.
+    End="X"
+    Start="O"
+    start_pos=find_start(maze,Start) #find the start position in the maze.the start_pos hold the tuple of postion.
+
+    q=queue.Queue()#initlize queue data structure.
+    #queue holds the tuple of start position and the list which hold the path to reach the end_pos
+    q.put((start_pos,[start_pos]))
+    visited=set() #A set (visited) keeps track of visited positions to avoid cycles.
 
 
 # stdscr is called standard screen which represent the window screen of the terminal all the inputs ,images or graphics manges in this screen only.
